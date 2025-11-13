4bd788a2d33b240f64d9db03903474376793cce6
nm2tech-llc <nm2tech77@gmail.com>
2025-11-13 15:50:28 -0500

    Fix: Disable image optimization to fix logo display issue with basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/nm2timesheet',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: If you're using a trailing slash
+  // trailingSlash: true,
 }
 
 module.exports = nextConfig
