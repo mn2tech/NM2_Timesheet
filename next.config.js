b8c92a6dca742de88d2fc8113a1ccc445d1007db
nm2tech-llc <nm2tech77@gmail.com>
2025-11-14 12:26:43 -0500

    Fix: Add basePath configuration for /nm2timesheet route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/nm2timesheet',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
